20de0aac6051843d446ae56a8e092df395098ed6
giovanavbs <giovana.santos261@etec.sp.gov.br>
2023-09-15 08:38:31 -0300

    att

---
+++ aplicacao_ds/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using aplicacao_ds.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aplicacao_ds.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        public ActionResult Index()
+        {
+            var cliente = new Cliente();
+            return View();
+        }
+        [HttpPost] 
+        public ActionResult Index(Cliente cliente)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", cliente);
+            } 
+            return View(cliente);
+        }
+        public ActionResult Resultado(Cliente cliente)
+        {
+            return View(cliente);
+        }
+    }
+}
